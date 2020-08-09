188c20ed15ac0773bcf3ed043af8b67a60a15a4e
Noob0799 <uchihaitachi0799@gmail.com>
2020-08-09 17:25:46 +0530

    Updates

---
+++ precache-manifest.fc84ee72c6fbd9c0dd328a57ce011a4f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61df1311b1ccf551d823a56befc9ccad",
+    "url": "/FoodMart/index.html"
+  },
+  {
+    "revision": "7a0c87d66c02775a1bde",
+    "url": "/FoodMart/static/css/2.45836d5f.chunk.css"
+  },
+  {
+    "revision": "2d44afb976062b341e97",
+    "url": "/FoodMart/static/css/main.f980352c.chunk.css"
+  },
+  {
+    "revision": "7a0c87d66c02775a1bde",
+    "url": "/FoodMart/static/js/2.76f016b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/FoodMart/static/js/2.76f016b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d44afb976062b341e97",
+    "url": "/FoodMart/static/js/main.1ad9c4b1.chunk.js"
+  },
+  {
+    "revision": "95275719205cbf07d4e5",
+    "url": "/FoodMart/static/js/runtime-main.365ca9b5.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/FoodMart/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/FoodMart/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/FoodMart/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/FoodMart/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/FoodMart/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/FoodMart/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/FoodMart/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/FoodMart/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/FoodMart/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/FoodMart/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/FoodMart/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/FoodMart/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/FoodMart/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/FoodMart/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/FoodMart/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/FoodMart/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/FoodMart/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
